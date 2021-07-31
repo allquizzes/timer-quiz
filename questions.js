328411b3f1c41daf408278b778eb3c8872ac53a3
Mohammad Omar Farooq Khan <72504380+allquizzes@users.noreply.github.com>
2021-07-31 13:01:28 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,1 @@
-const qu = [ ["Question"  , "O" , "O" , 1] , ["Q", "O" , "O" , 0 ] ] ;
+const qu = [ ["87 is divisible by 21"  , "True" , "False" , 0] , ["Largest Ocean", "Pacific Ocean" , "Atlantic Ocean" , 0 ] ] ;
