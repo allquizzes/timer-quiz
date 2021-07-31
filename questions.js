48fd3b53a22385ba9e561c13abf07b563c04ff8f
Mohammad Omar Farooq Khan <72504380+allquizzes@users.noreply.github.com>
2021-07-31 16:10:24 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,1 @@
-const qu = [ ["81 is divisible by 27"  , "True" , "False" , 0] , ["Largest Ocean", "Pacific Ocean" , "Atlantic Ocean" , 0 ] ] ;
+const qu = [ ["81 is divisible by 27"  , "True" , "False" , 0] , ["Largest Ocean", "Pacific Ocean" , "Atlantic Ocean" , 0 ] , ["Largest Desert", "Sahara Desert" , "Antarctic Desert" , 1 ] , ["Largest internal organ of human body" , "Lungs", "Liver" , 1 ] , ["Capital of Denmark" , "Copenhagen" , "Berlin" , 0 ] ] ;
